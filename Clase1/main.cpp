e225df0b9dbd1f5f98d0bd0444d2b41c05cad325
Alejandro Avila <alejandroavila@MacBook-Air-de-Alejandro.local>
2022-11-17 00:00:48 +0100

    Clase1

---
+++ Clase1/main.cpp
@@ -0,0 +1,27 @@
+int main(int argc, char const *argv[])
+{
+    // Tipos de variables.
+
+    // Enteros
+    int entero = 10;
+    short enteroCorto = 10;
+    long enteroLargo = 10;
+    long long enteroLargoLargo = 10;
+
+    // Decimales
+    float decimal = 10.5;
+    double decimalDoble = 10.5;
+    long double decimalLargo = 10.5;
+
+    // Caracteres
+    char caracter = 'a';
+    char caracter2 = 97;
+
+    // Booleanos
+    bool booleano = true;
+
+    // Constantes
+    const int constante = 10;
+    
+    return 0;
+}
